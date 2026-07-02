311497fc166e9d7d8f0ae9b9d96a02f50fe06f72
vanshikaaaa0104-ai <vanshikaaaa0104@gmail.com>
2026-07-02 15:21:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("StayEase Loaded Successfully!");
+
+
+const searchBtn = document.querySelector(".search-btn");
+
+searchBtn.addEventListener("click", function () {
+
+    alert("Searching hotels...");
+
+});
